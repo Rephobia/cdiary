422f667814b57f16b843b66efa85eadcfaafd3aa
Rephobia <26533256+Rephobia@users.noreply.github.com>
2022-01-09 23:54:24 +0300

    Add typescript config

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,23 @@ module.exports = {
         port: 8090,
     },
     entry: {
-        main: path.resolve(__dirname, './src/index.js'),
+        main: path.resolve(__dirname, './src/index.ts'),
     },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
+    },
+    module: {
+	rules: [
+	    {
+		test: /\.tsx?$/,
+		use: 'ts-loader',
+		exclude: /node_modules/,
+	    },
+	],
+    },
+    resolve: {
+	extensions: ['.tsx', '.ts'],
     },
     plugins: [
         new HtmlWebpackPlugin({
